e23ee710c88c59970c0712a57cb3cd073cd8f7a9
鲨鱼辣椒 <monishine_code@163.com>
2025-06-20 09:23:39 +0800

    feat: commit

--- app/api/routes/plate.py
+++ app/api/routes/plate.py
@@ -1,7 +1,99 @@
 from fastapi import APIRouter
+import requests
+import datetime
+import pandas as pd
+import json
 
 
 router = APIRouter()
+
+
+def get_plate_data():
+    url1 = "xxx"
+    url2 = "xxx"
+
+    headers = {
+        "Host": "XXXX" if k == 1 else "XXX",
+        "Content-Type": "application/x-www-form-urlencoded; charset=utf-8",
+        "Connection": "keep-alive",
+        "Accept": "*/*",
+        "User-Agent": "lhb/5.17.9 (XXXX; build:0; iOS 16.6.0) Alamofire/4.9.1",
+        "Accept-Language": "zh-Hans-CN;q=1.0",
+        "Accept-Encoding": "gzip;q=1.0, compress;q=0.5",
+    }
+    params = {
+        "Date": date if k == 1 else datetime.date.today().strftime("%Y-%m-%d"),
+        "Index": "0",
+        "Order": "1",
+        "PhoneOSNew": "2",
+        "Type": "1",
+        "VerSion": "5.17.0.9",
+        "ZSType": "7",
+        "a": "XXXXXX",
+        "apiv": "w38",
+        "c": "XXXXX",
+        "st": "20",
+    }
+    url = url1 if k == 0 else url2
+    print(f"请求地址：{url}")
+    print(f"请求参数：{params}")
+
+    try:
+        response = requests.post(url=url, headers=headers, data=params)
+        if response.status_code == 200:
+            data = response.json()
+            if "list" in data and data["list"]:
+                plate_list = []
+                for item in data["list"]:
+                    if len(item) >= 4:
+                        plate_list.append(
+                            {
+                                "代码": item[0],
+                                "名称": item[1],
+                                "强度": item[2],
+                                "涨幅%": item[3],
+                            }
+                        )
+                return plate_list
+            else:
+                print("数据缺失")
+        else:
+            print(f"接口请求失败：{response.status_code}")
+    except Exception as err:
+        print(f"数据获取失败：{str(err)}")
+
+
+def get_stock_data(stock_code: str, date: str, k: int = 0):
+    url1 = "xxx"
+    url2 = "xxx"
+    headers = {
+        "Host": "XXXX" if k == 1 else "XXXX",
+        "Content-Type": "application/x-www-form-urlencoded; charset=utf-8",
+        "Connection": "keep-alive",
+        "Accept": "*/*",
+        "User-Agent": "lhb/5.17.9 (XXXX; build:0; iOS 16.6.0) Alamofire/4.9.1",
+        "Accept-Language": "zh-Hans-CN;q=1.0",
+        "Accept-Encoding": "gzip;q=1.0, compress;q=0.5",
+    }
+    params = {
+        "PlateID": sector_code,
+        "Date": date if k == 1 else datetime.date.today().strftime("%Y-%m-%d"),
+        "Index": "0",
+        "Order": "1",
+        "PhoneOSNew": "2",
+        "Type": "6",
+        "VerSion": "5.17.0.9",
+        "a": "XXXXX",
+        "apiv": "w38",
+        "c": "XXXXX",
+        "st": "20",
+    }
+    url = url1 if k == 0 else url2
+
+    try:
+        response = requests.post(url=url, headers=headers, data=params)
+    except Exception as err:
+        print(f"获取个股数据失败：{str(err)}")
 
 
 @router.get("/")
